bb543988dbf7c75667a41ab6257d9f9d599c65ce
Anatoly Chernow <chertoly@gmail.com>
2017-12-05 19:36:25 +0200

    Add additional deb dependencies

--- provision/initial_provision.sh
+++ provision/initial_provision.sh
@@ -4,7 +4,7 @@ sudo -i
 apt-get update
 apt-get upgrade -y
 
-apt-get install ruby git -y
+apt-get install vim-nox ruby zsh tmux htop git less file -y
 
 echo "gem: --no-document" > /root/.gemrc
 gem i bundler
