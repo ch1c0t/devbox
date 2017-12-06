bd7ac314c9741086d2fe125f51f152c5444d7ce6
Anatoly Chernow <chertoly@gmail.com>
2017-12-06 23:25:32 +0200

    Install oh-my-zsh

--- provision/initial_provision.sh
+++ provision/initial_provision.sh
@@ -8,3 +8,5 @@ apt-get install vim-nox ruby zsh tmux htop git less file -y
 
 echo "gem: --no-document" > /root/.gemrc
 gem i bundler docker-api
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
