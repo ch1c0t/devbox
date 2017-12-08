a1b85e7b9dbaf7db4a8d9a94f8b675c089e89605
Anatoly Chernow <chertoly@gmail.com>
2017-12-08 23:46:50 +0200

    Set up vim config

--- provision.sh
+++ provision.sh
@@ -10,3 +10,6 @@ echo "gem: --no-document" > /root/.gemrc
 gem i bundler docker-api
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+git clone --recursive https://github.com/ch1c0t/.vim
+ln -s .vim/main.vim .vimrc
