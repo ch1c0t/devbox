702fe1eec505f3ae9e1fc985e8e1935211d906f9
Anatoly Chernow <chertoly@gmail.com>
2017-12-09 23:46:41 +0200

    Set up vim for root user

--- provision.sh
+++ provision.sh
@@ -9,7 +9,8 @@ apt-get install vim-nox ruby zsh tmux htop git less file -y
 echo "gem: --no-document" > /root/.gemrc
 gem i bundler docker-api
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 git clone --recursive https://github.com/ch1c0t/.vim
 ln -s .vim/main.vim .vimrc
+
+# This command changes from root to vagrant user.
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
